43ec21dbb72fd4547cf26e0d3448a6cc6c00e200
kannadan <joonas.sou@gmail.com>
2017-11-22 19:30:18 +0200

    Leagues and avarage price counter added

--- firebase.py
+++ firebase.py
@@ -3,6 +3,7 @@ import json
 import requests
 import time
 from threading import Thread, RLock, Event
+import numpy as np
 
 
 
@@ -11,6 +12,7 @@ from threading import Thread, RLock, Event
 
 values = {}
 exit = Event()
+
 
 
 def authData():
@@ -66,358 +68,55 @@ def saver(exit):
     print("saver dead")
 
 def empty():
-    values = {
-        "Orb of Alteration" : {
-                    "times" : 0,
-                    'chaos' : [],
-                    'exa' : [],
-                    'alch' : [],
-                    'chisel' : [],
-                    'fuse': [],
-                    'alt' : [],
-                    'vaal' : [],
-                    'exalted' : [],
-                    'jew' : []
-        },
-        "Orb of Fusing" : {
-                    "times" : 0,
-                    'chaos' : [],
-                    'exa' : [],
-                    'alch' : [],
-                    'chisel' : [],
-                    'fuse': [],
-                    'alt' : [],
-                    'vaal' : [],
-                    'exalted' : [],
-                    'jew' : []
-        },
-        "Orb of Alchemy" : {
-                    "times" : 0,
-                    'chaos' : [],
-                    'exa' : [],
-                    'alch' : [],
-                    'chisel' : [],
-                    'fuse': [],
-                    'alt' : [],
-                    'vaal' : [],
-                    'exalted' : [],
-                    'jew' : []
-        },
-        "Chaos Orb" : {
-                    "times" : 0,
-                    'chaos' : [],
-                    'exa' : [],
-                    'alch' : [],
-                    'chisel' : [],
-                    'fuse': [],
-                    'alt' : [],
-                    'vaal' : [],
-                    'exalted' : [],
-                    'jew' : []
-        },
-        "Gemcutter's Prism" : {
-                    "times" : 0,
-                    'chaos' : [],
-                    'exa' : [],
-                    'alch' : [],
-                    'chisel' : [],
-                    'fuse': [],
-                    'alt' : [],
-                    'vaal' : [],
-                    'exalted' : [],
-                    'jew' : []
-        },
-        "Exalted Orb" : {
-                    "times" : 0,
-                    'chaos' : [],
-                    'exa' : [],
-                    'alch' : [],
-                    'chisel' : [],
-                    'fuse': [],
-                    'alt' : [],
-                    'vaal' : [],
-                    'exalted' : [],
-                    'jew' : []
-        },
-        "Chromatic Orb" : {
-                    "times" : 0,
-                    'chaos' : [],
-                    'exa' : [],
-                    'alch' : [],
-                    'chisel' : [],
-                    'fuse': [],
-                    'alt' : [],
-                    'vaal' : [],
-                    'exalted' : [],
-                    'jew' : []
-        },
-        "Jeweller's Orb" : {
-                    "times" : 0,
-                    'chaos' : [],
-                    'exa' : [],
-                    'alch' : [],
-                    'chisel' : [],
-                    'fuse': [],
-                    'alt' : [],
-                    'vaal' : [],
-                    'exalted' : [],
-                    'jew' : []
-        },
-        "Orb of Chance" : {
-                    "times" : 0,
-                    'chaos' : [],
-                    'exa' : [],
-                    'alch' : [],
-                    'chisel' : [],
-                    'fuse': [],
-                    'alt' : [],
-                    'vaal' : [],
-                    'exalted' : [],
-                    'jew' : []
-        },
-        "Cartographer's Chisel" : {
-                    "times" : 0,
-                    'chaos' : [],
-                    'exa' : [],
-                    'alch' : [],
-                    'chisel' : [],
-                    'fuse': [],
-                    'alt' : [],
-                    'vaal' : [],
-                    'exalted' : [],
-                    'jew' : []
-        },
-        "Orb of Scouring" : {
-                    "times" : 0,
-                    'chaos' : [],
-                    'exa' : [],
-                    'alch' : [],
-                    'chisel' : [],
-                    'fuse': [],
-                    'alt' : [],
-                    'vaal' : [],
-                    'exalted' : [],
-                    'jew' : []
-        },
-        "Blessed Orb" : {
-                    "times" : 0,
-                    'chaos' : [],
-                    'exa' : [],
-                    'alch' : [],
-                    'chisel' : [],
-                    'fuse': [],
-                    'alt' : [],
-                    'vaal' : [],
-                    'exalted' : [],
-                    'jew' : []
-        },
-        "Orb of Regret" : {
-                    "times" : 0,
-                    'chaos' : [],
-                    'exa' : [],
-                    'alch' : [],
-                    'chisel' : [],
-                    'fuse': [],
-                    'alt' : [],
-                    'vaal' : [],
-                    'exalted' : [],
-                    'jew' : []
-        },
-        "Regal Orb" : {
-                    "times" : 0,
-                    'chaos' : [],
-                    'exa' : [],
-                    'alch' : [],
-                    'chisel' : [],
-                    'fuse': [],
-                    'alt' : [],
-                    'vaal' : [],
-                    'exalted' : [],
-                    'jew' : []
-        },
-        "Divine Orb" : {
-                    "times" : 0,
-                    'chaos' : [],
-                    'exa' : [],
-                    'alch' : [],
-                    'chisel' : [],
-                    'fuse': [],
-                    'alt' : [],
-                    'vaal' : [],
-                    'exalted' : [],
-                    'jew' : []
-        },
-        "Vaal Orb" : {
-                    "times" : 0,
-                    'chaos' : [],
-                    'exa' : [],
-                    'alch' : [],
-                    'chisel' : [],
-                    'fuse': [],
-                    'alt' : [],
-                    'vaal' : [],
-                    'exalted' : [],
-                    'jew' : []
-        },
-        "Orb of Augmentation" : {
-                    "times" : 0,
-                    'chaos' : [],
-                    'exa' : [],
-                    'alch' : [],
-                    'chisel' : [],
-                    'fuse': [],
-                    'alt' : [],
-                    'vaal' : [],
-                    'exalted' : [],
-                    'jew' : []
-        },
-        "Eternal Orb" : {
-                    "times" : 0,
-                    'chaos' : [],
-                    'exa' : [],
-                    'alch' : [],
-                    'chisel' : [],
-                    'fuse': [],
-                    'alt' : [],
-                    'vaal' : [],
-                    'exalted' : [],
-                    'jew' : []
-        },
-        "Scroll of Wisdom" : {
-                    "times" : 0,
-                    'chaos' : [],
-                    'exa' : [],
-                    'alch' : [],
-                    'chisel' : [],
-                    'fuse': [],
-                    'alt' : [],
-                    'vaal' : [],
-                    'exalted' : [],
-                    'jew' : []
-        },
-        "Scroll of Portal" : {
-                    "times" : 0,
-                    'chaos' : [],
-                    'exa' : [],
-                    'alch' : [],
-                    'chisel' : [],
-                    'fuse': [],
-                    'alt' : [],
-                    'vaal' : [],
-                    'exalted' : [],
-                    'jew' : []
-        },
-        "Mirror of Kalandra" : {
-                    "times" : 0,
-                    'chaos' : [],
-                    'exa' : [],
-                    'alch' : [],
-                    'chisel' : [],
-                    'fuse': [],
-                    'alt' : [],
-                    'vaal' : [],
-                    'exalted' : [],
-                    'jew' : []
-        },
-        "Silver Coin" : {
-                    "times" : 0,
-                    'chaos' : [],
-                    'exa' : [],
-                    'alch' : [],
-                    'chisel' : [],
-                    'fuse': [],
-                    'alt' : [],
-                    'vaal' : [],
-                    'exalted' : [],
-                    'jew' : []
-        },
-        "Blacksmith's Whetstone" : {
-                    "times" : 0,
-                    'chaos' : [],
-                    'exa' : [],
-                    'alch' : [],
-                    'chisel' : [],
-                    'fuse': [],
-                    'alt' : [],
-                    'vaal' : [],
-                    'exalted' : [],
-                    'jew' : []
-        },
-        "Armourer's Scrap" : {
-                    "times" : 0,
-                    'chaos' : [],
-                    'exa' : [],
-                    'alch' : [],
-                    'chisel' : [],
-                    'fuse': [],
-                    'alt' : [],
-                    'vaal' : [],
-                    'exalted' : [],
-                    'jew' : []
-        },
-        "Glassblower's Bauble" : {
-                    "times" : 0,
-                    'chaos' : [],
-                    'exa' : [],
-                    'alch' : [],
-                    'chisel' : [],
-                    'fuse': [],
-                    'alt' : [],
-                    'vaal' : [],
-                    'exalted' : [],
-                    'jew' : []
-        },
-        "Orb of transmutation" : {
-                    "times" : 0,
-                    'chaos' : [],
-                    'exa' : [],
-                    'alch' : [],
-                    'chisel' : [],
-                    'fuse': [],
-                    'alt' : [],
-                    'vaal' : [],
-                    'exalted' : [],
-                    'jew' : []
-        },
-        "Apprentice Cartographer's Sextant" : {
-                    "times" : 0,
-                    'chaos' : [],
-                    'exa' : [],
-                    'alch' : [],
-                    'chisel' : [],
-                    'fuse': [],
-                    'alt' : [],
-                    'vaal' : [],
-                    'exalted' : [],
-                    'jew' : []
-        },
-        "Journeyman Cartographer's Sextant" : {
-                    "times" : 0,
-                    'chaos' : [],
-                    'exa' : [],
-                    'alch' : [],
-                    'chisel' : [],
-                    'fuse': [],
-                    'alt' : [],
-                    'vaal' : [],
-                    'exalted' : [],
-                    'jew' : []
-        },
-        "Master Cartographer's Sextant" : {
-                    "times" : 0,
-                    'chaos' : [],
-                    'exa' : [],
-                    'alch' : [],
-                    'chisel' : [],
-                    'fuse': [],
-                    'alt' : [],
-                    'vaal' : [],
-                    'exalted' : [],
-                    'jew' : []
-        },
+    curs = {
+                "times" : 0,
+                'chaos' : [],
+                'exa' : [],
+                'alch' : [],
+                'chisel' : [],
+                'fuse': [],
+                'alt' : [],
+                'vaal' : [],
+                'exalted' : [],
+                'jew' : []
+    }
+    everything = {
+        "Orb of Alteration" : curs.copy(),
+        "Orb of Fusing" : curs.copy(),
+        "Orb of Alchemy" : curs.copy(),
+        "Chaos Orb" : curs.copy(),
+        "Gemcutter's Prism" : curs.copy(),
+        "Exalted Orb" : curs.copy(),
+        "Chromatic Orb" : curs.copy(),
+        "Jeweller's Orb" : curs.copy(),
+        "Orb of Chance" : curs.copy(),
+        "Cartographer's Chisel" : curs.copy(),
+        "Orb of Scouring" : curs.copy(),
+        "Blessed Orb" : curs.copy(),
+        "Orb of Regret" : curs.copy(),
+        "Regal Orb" : curs.copy(),
+        "Divine Orb" : curs.copy(),
+        "Vaal Orb" : curs.copy(),
+        "Orb of Augmentation" : curs.copy(),
+        "Eternal Orb" : curs.copy(),
+        "Scroll of Wisdom" : curs.copy(),
+        "Scroll of Portal" : curs.copy(),
+        "Mirror of Kalandra" : curs.copy(),
+        "Silver Coin" : curs.copy(),
+        "Blacksmith's Whetstone" : curs.copy(),
+        "Armourer's Scrap" : curs.copy(),
+        "Glassblower's Bauble" : curs.copy(),
+        "Orb of transmutation" : curs.copy(),
+        "Apprentice Cartographer's Sextant" : curs.copy(),
+        "Journeyman Cartographer's Sextant" : curs.copy(),
+        "Master Cartographer's Sextant" : curs.copy()
 
     }
-    return values
+    leagues = ["Standard", "Hardcore", "SSF Harbinger HC", "Hardcore Harbinger", "SSF Harbinger", "Harbinger"]
+    finale = {}
+    for name in leagues:
+        finale[name] = everything.copy()
+    return finale
 
 
 
@@ -437,7 +136,7 @@ converted = {
 }
 lock = RLock()
 def main(exit):
-    numb = "109046044-114348493-107307633-123646289-115642409"
+    numb = "109120935-114428894-107376749-123741778-115720700"
     id = "?id=" + numb
     sites = 0
     stashes = 0
@@ -464,15 +163,20 @@ def main(exit):
         for stash in data["stashes"]:
             if stash["public"]:
                 for item in stash["items"]:
-                    if "note" in item.keys() and item["typeLine"] in values.keys():
+                    if "note" in item.keys() and item["typeLine"] in values["Harbinger"].keys():
                         #print("note: {}".format(item["note"]))
                         line = item["note"]
                         price = getPrice(line)
                         if price != None:
                             lock.acquire()
                             try:
-                                values[item["typeLine"]][price[1]].append(price[0])
-                                values[item["typeLine"]]["times"] += 1
+                                values[item["league"]][item["typeLine"]][price[1]].append(price[0])
+                                values[item["league"]][item["typeLine"]]["times"] += 1
+                            except KeyError:
+                                with open("errors.txt", "a") as kohde:
+                                    kohde.write("league: {}\ntypeline: {}\nprice1: {}".format(item["league"], item["typeLine"], price[1]))
+                                lock.release()
+                                continue
                             finally:
                                 lock.release()
                             print("{} is prized at {} {}".format(item["typeLine"], price[0], price[1]))
@@ -481,14 +185,74 @@ def main(exit):
         id = "?id=" + data["next_change_id"]
         sites += 1
 
+
         print(data["next_change_id"])
         print("Stashes {}".format(len(data["stashes"])))
         timeR = time.time() - timeS
         print("Pages visited {}\nStashes mined: {}\nTime run: {}\n".format(sites, stashes, timeR))
 
+def findleague(exit):
+    numb = "109116329-114424267-107372105-123736579-115715272"
+    id = "?id=" + numb
+    sites = 0
+    stashes = 0
+    leagues = {}
+    while not exit.is_set():
+        try:
+            resp = requests.get("http://api.pathofexile.com/public-stash-tabs" + id)
+            print("got some")
+        except requests.exceptions.RequestException as e:  # This is the correct syntax
+            print("ERROR OCCURED REQUESTING THE SITE, TRYING AGAIN IN 2 SECONDS")
+            time.sleep(2)
+            continue
+        data = json.loads(resp.text)
+        if not "next_change_id" in data.keys():
+            print("HIT ROCK BOTTOM, TRYING AGAIN")
+            continue
+        for stash in data["stashes"]:
+            if stash["public"]:
+                for item in stash["items"]:
+                    if item["league"] not in leagues.keys():
+                        leagues[item["league"]] = 1
+                    else:
+                        leagues[item["league"]] += 1
+
+        stashes += len(data["stashes"])
+        id = "?id=" + data["next_change_id"]
+        sites += 1
+        print(data["next_change_id"])
+        print("Stashes {}".format(len(data["stashes"])))
+        for i in leagues:
+            print("League: {}\n\t{}".format(i, leagues[i]))
+        print("Pages visited {}\nStashes mined: {}\n".format(sites, stashes))
+
+def avarages(data):
+    avrg = {}
+    for league in data:
+        avrg[league] = {}
+        for item in data[league]:
+            chaos = data[league][item]["chaos"]
+            chaos = np.array(chaos)
+            if len(chaos) != 0:
+                mean = np.mean(chaos)
+                std = np.std(chaos)
+            else:
+                mean = 0
+            if mean == 0:
+                avrg[league][item] = mean
+            else:
+                final = [x for x in chaos if (x > mean - 1.5 * std)]
+                final = [x for x in final if (x < mean + 1.5 * std)]
+                avrg[league][item] = np.mean(np.array(final))
+
+    return avrg
+
+def postData(data):
+    pass
 
 if __name__ == "__main__":
-    values = empty()
+
+    """values = empty()
     t = Thread(target=main,  args=(exit,))
     t2 = Thread(target=saver,  args=(exit,))
     t.start()
@@ -498,4 +262,11 @@ if __name__ == "__main__":
             time.sleep(1)
         except KeyboardInterrupt:
             exit.set()
-            break
+            break"""
+    data = json.load(open("22-11-2017_16-56-33_poe.json", "r"))
+    data = avarages(data)
+    with open("tulokset.json", 'w') as fp:
+        json.dump(data, fp)
+    firebase = configure()
+    db = firebase.database()
+    db.set({"Harbinger" : data["harbinger"]})
